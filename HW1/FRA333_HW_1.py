07cf199b10a787b6136826c4cfbf5877dd36a58f
omzlette <tharnarch.omz@mail.kmutt.ac.th>
2022-09-15 10:09:05 +0700

    can walk correctly (pos on xy)

--- HW1/FRA333_HW_1.py
+++ HW1/FRA333_HW_1.py
@@ -9,8 +9,11 @@ import json
 class MyBeeBot(BeeBot):
     def __init__(self,a_i):
         super().__init__(a_i)
-        self.walkedPath = self.currentPos = np.array(a_i).reshape(2,1)
-        self.theta = (60*np.pi)/180
+        self.walkedPath = self.currentPos = self.idx2pos(a_i[0],a_i[1]).reshape(2,1)
+        # self.walkedPath = self.currentPos = np.array(a_i).reshape(2,1)
+        self.thetaL = (60*np.pi)/180
+        self.thetaR = (-60*np.pi)/180
+        print(self.currentPos)
     # a_i is initial position
     # c is command {'0'->stop, '1'->forward, '2'->backward, '3'->turn right, '4'->turn left}
     # o is obstacle
@@ -19,33 +22,39 @@ class MyBeeBot(BeeBot):
     # To Y-Axis: use RotateMatX | To X-Axis: use RotateMatZ | To Z-Axis: use RotateMatY
     # On Y: XZ | On X: YZ | On Z: XY
     
-    def rotateMatX(self, theta):
-        return np.array([[1, 0, 0],
-                         [0, np.cos(theta), -np.sin(theta)],
-                         [0, np.sin(theta), np.cos(theta)]])
+    # def rotateMatX(self, theta):
+    #     return np.array([[1, 0, 0],
+    #                      [0, np.cos(theta), -np.sin(theta)],
+    #                      [0, np.sin(theta), np.cos(theta)]])
     
-    def rotateMatY(self, theta):
-        return np.array([[np.cos(theta), 0, np.sin(theta)],
-                         [0, 1, 0],
-                         [-np.sin(theta), 0, np.cos(theta)]])
+    # def rotateMatY(self, theta):
+    #     return np.array([[np.cos(theta), 0, np.sin(theta)],
+    #                      [0, 1, 0],
+    #                      [-np.sin(theta), 0, np.cos(theta)]])
     
-    def rotateMatZ(self, theta):
-        return np.array([[np.cos(theta), -np.sin(theta), 0],
-                         [np.sin(theta), np.cos(theta), 0],
-                         [0, 0, 1]])
+    # def rotateMatZ(self, theta):
+    #     return np.array([[np.cos(theta), -np.sin(theta), 0],
+    #                      [np.sin(theta), np.cos(theta), 0],
+    #                      [0, 0, 1]])
+    
+    def rotateMat(self, theta):
+        return np.array([[np.cos(theta), -np.sin(theta)],
+                         [np.sin(theta), np.cos(theta)]])
     
     def wallCollision(self, W):
         if len(W) != 0:
             pass
         return False
     
+    def pos2idx(self,x,y):
+        pass
+    
     def trackBeeBot(self, com, W):
         #get all commands that are not 0
         com = [i for i in com if i != '0']
         # get all obstacles (walls)
         W = np.array(W)
-        stepMatrix = np.array([[1],
-                               [1]])
+        stepMatrix = self.idx2pos(1,1).reshape(2,1)
         if len(com) == 0:
             return self.currentPos, 0
         
@@ -61,10 +70,10 @@ class MyBeeBot(BeeBot):
                 self.walkedPath = np.append(self.walkedPath, self.currentPos, axis=1)
                 print("backward")
             elif i == '3':
-                stepMatrix = np.matmul(self.rotateMatL(self.theta), stepMatrix)
+                stepMatrix = np.matmul(self.rotateMat(self.thetaL), stepMatrix)
                 print("left")
             elif i == '4':
-                stepMatrix = np.matmul(self.rotateMatR(self.theta), stepMatrix)
+                stepMatrix = np.matmul(self.rotateMat(self.thetaR), stepMatrix)
                 print("right")
             # else:
             #     print("Invalid command")
@@ -78,5 +87,5 @@ class MyBeeBot(BeeBot):
     
     # add your code here
 testBot = MyBeeBot([-4, -1])
-A = testBot.trackBeeBot('10004031304133412113203401202434122434340313312420340143230302444311410144134242324403414413032441433', [])
-print(A)
+P = testBot.trackBeeBot('3', [])
+print(P)
--- HW1/test.py
+++ HW1/test.py
@@ -1,19 +1,19 @@
 import numpy as np
 
-walkedPath = a_i = np.array([-4, -1]).reshape(2,1) # facing north
-stepMatrix = np.array([[1],[0],[1],[0]]) # XYZ
+# walkedPath = a_i = np.array([-4, -1]).reshape(2,1) # facing north
+# stepMatrix = np.array([[1],[1]]) # XYZ
 # turn left
-theta = (-90*np.pi)/180
+thetaL = (90*np.pi)/180
 # turn right
 # thetaR = (-45*np.pi)/180
 
 # COM = ""
 
-# def testRotateMat(theta):
-#     # rotation matrix
-#     R = np.array([[np.cos(theta), -np.sin(theta)],
-#                   [np.sin(theta), np.cos(theta)]])
-#     return R
+def testRotateMat(theta):
+    # rotation matrix
+    R = np.array([[np.cos(theta), -np.sin(theta)],
+                  [np.sin(theta), np.cos(theta)]])
+    return R
 
 # def rotateMatX(theta):
 #     return np.array([[1, 0, 0],
@@ -30,36 +30,42 @@ theta = (-90*np.pi)/180
 #                         [np.sin(theta), np.cos(theta), 0],
 #                         [0, 0, 1]])
 
-# stepMatrix = np.matmul(rotateMatZ(theta), stepMatrix)
+def idx2pos(i,j):
+  d = 1
+  a = np.array([3/2,np.math.sqrt(3)/2])*d*(i-1)
+  b = np.array([-3/2,np.math.sqrt(3)/2])*d*(j-1)
+  return a+b+np.array([0,np.math.sqrt(3)])
 
-# print(stepMatrix)
+testArr = idx2pos(2,2).reshape(2,1)
+
+print(testArr)
 # # print([i for i in COM if i != '0'])
 
-def radians(angle):
-  return angle * np.pi / 180
+# def radians(angle):
+#   return angle * np.pi / 180
 
-def trig(angle):
-  r = radians(angle)
-  return np.cos(r), np.sin(r)
+# def trig(angle):
+#   r = radians(angle)
+#   return np.cos(r), np.sin(r)
 
-def matrix(rotation):
-  xC, xS = trig(rotation[0])
-  yC, yS = trig(rotation[1])
-  zC, zS = trig(rotation[2])
-  Rotate_X_matrix = np.array([[1, 0, 0, 0],
-                              [0, xC, -xS, 0],
-                              [0, xS, xC, 0],
-                              [0, 0, 0, 1]])
-  Rotate_Y_matrix = np.array([[yC, 0, yS, 0],
-                              [0, 1, 0, 0],
-                              [-yS, 0, yC, 0],
-                              [0, 0, 0, 1]])
-  Rotate_Z_matrix = np.array([[zC, -zS, 0, 0],
-                              [zS, zC, 0, 0],
-                              [0, 0, 1, 0],
-                              [0, 0, 0, 1]])
-  return np.dot(Rotate_Z_matrix,np.dot(Rotate_Y_matrix, Rotate_X_matrix))
+# def matrix(rotation):
+#   xC, xS = trig(rotation[0])
+#   yC, yS = trig(rotation[1])
+#   zC, zS = trig(rotation[2])
+#   Rotate_X_matrix = np.array([[1, 0, 0, 0],
+#                               [0, xC, -xS, 0],
+#                               [0, xS, xC, 0],
+#                               [0, 0, 0, 1]])
+#   Rotate_Y_matrix = np.array([[yC, 0, yS, 0],
+#                               [0, 1, 0, 0],
+#                               [-yS, 0, yC, 0],
+#                               [0, 0, 0, 1]])
+#   Rotate_Z_matrix = np.array([[zC, -zS, 0, 0],
+#                               [zS, zC, 0, 0],
+#                               [0, 0, 1, 0],
+#                               [0, 0, 0, 1]])
+#   return np.dot(Rotate_Z_matrix,np.dot(Rotate_Y_matrix, Rotate_X_matrix))
 
-stepMatrix = np.matmul(matrix([0,0,theta],[0,0,0]), stepMatrix)
-print(stepMatrix)
+# stepMatrix = np.matmul(matrix([0,0,theta],[0,0,0]), stepMatrix)
+# print(stepMatrix)
 # print(matrix([0,0,90],[1,1,0]))
