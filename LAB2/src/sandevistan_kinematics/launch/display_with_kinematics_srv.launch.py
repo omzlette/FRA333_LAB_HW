5d9572e13e6078dff39dde763939027142e233a5
omzlette <tharnarch.omz@mail.kmutt.ac.th>
2022-09-30 18:09:58 +0700

    can run now yay

--- LAB2/src/sandevistan_kinematics/launch/display_with_kinematics_srv.launch.py
+++ LAB2/src/sandevistan_kinematics/launch/display_with_kinematics_srv.launch.py
@@ -2,7 +2,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
 from launch.substitutions import PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
@@ -28,13 +28,23 @@ def generate_launch_description():
     
     kinematics_server = Node(package='sandevistan_kinematics',
                              executable='kinematics_server.py',)
-                             
+    
+    q1, q2, q3, q4 = 30.0, 180.0, 60.0, 0.03
+                    
+    srv_jointState = ExecuteProcess(
+        cmd=[[
+            'ros2 service call /set_joints sandevistan_kinematics_interfaces/srv/GetPosition ',
+            f'"{{joint: {{position: [{q1},{q2},{q3},{q4}]}}}}"'
+        ]],
+        shell=True)
+    
     
     # Launch Description
     launch_description = LaunchDescription()
     launch_description.add_action(display)
     launch_description.add_action(kinematics_server)
-
+    launch_description.add_action(srv_jointState)
+    
     return launch_description
 
 def main(args=None):
--- LAB2/src/sandevistan_kinematics/scripts/kinematics_server.py
+++ LAB2/src/sandevistan_kinematics/scripts/kinematics_server.py
@@ -24,6 +24,9 @@ class SandeStatePub(Node):
         self.jointState = JointState()
         self.jointState.position = [0., 0., 0., 0., 0., 0.]
         
+        self.linkLengths = [0.4, 0.15, 0.2]
+        self.degree = np.pi / 180.0
+        
         self.setJoint = self.create_service(GetPosition, 'set_joints', self.setJointCallback)
         
         self.get_logger().info("SandeStatePub started")
@@ -33,12 +36,27 @@ class SandeStatePub(Node):
         self.jointState.name = ['joint_base_0', 'joint_0_1', 'joint_1_2',
                                 'joint_2_3', 'joint_3_4', 'joint_4_neff']
         self.jointPub.publish(self.jointState)
-        
-    def setJointCallback(self, request, response):
-        jointPos = request.joint.position
-        self.jointState.position = np.array([[(-l1 - l2 - q4)*np.sin(q1)*np.cos(q2)],
-                                             [(l1 + l2 + q4)*np.cos(q1)*np.cos(q2)],
-                                             [l0 + l1*np.sin(q2) + l2*np.sin(q2) + q4*np.sin(q2)]])
+
+    # def posMat(self, q1, q2, q3, q4):
+    #     positionMat = np.array([[(-self.linkLengths[1] - self.linkLengths[2] - q4)*np.sin(q1)*np.cos(q2)],
+    #                             [(self.linkLengths[1] + self.linkLengths[2] + q4)*np.cos(q1)*np.cos(q2)],
+    #                             [self.linkLengths[0] + self.linkLengths[1]*np.sin(q2) + self.linkLengths[2]*np.sin(q2) + q4*np.sin(q2)]])
+    #     return Point(x=positionMat[0, 0], y=positionMat[1, 0], z=positionMat[2, 0])
+    
+    def setJointCallback(self, request:GetPosition.Request, response:GetPosition.Response):
+        # request.joint.header.stamp = self.get_clock().now().to_msg()
+        request.joint.name = self.jointState.name
+        [q1, q2, q3, q4] = request.joint.position[0:4]
+        [q1, q2, q3] = [i*self.degree for i in [q1, q2, q3]]
+        self.jointState.position = [0., q1, q2, q3, q4, 0.]
+        self.get_logger().info('Incoming request\nPosition: ' + str(request.joint.position[0:4]))
+        positionMat = np.array([[(-self.linkLengths[1] - self.linkLengths[2] - q4)*np.sin(q1)*np.cos(q2)],
+                                [(self.linkLengths[1] + self.linkLengths[2] + q4)*np.cos(q1)*np.cos(q2)],
+                                [self.linkLengths[0] + self.linkLengths[1]*np.sin(q2) + self.linkLengths[2]*np.sin(q2) + q4*np.sin(q2)]])
+        self.get_logger().info('Position X: %.3f\nPosition Y: %.3f\nPosition Z: %.3f' % (positionMat[0, 0], positionMat[1, 0], positionMat[2, 0]))
+        response.position.x = positionMat[0,0]
+        response.position.y = positionMat[1,0]
+        response.position.z = positionMat[2,0]
         return response
 
 def main(args=None):
