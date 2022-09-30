75c492b3c3fb9e194005a9bf1f68eb281191a92e
omzlette <tharnarch.omz@mail.kmutt.ac.th>
2022-10-01 00:39:05 +0700

    remove comments

--- LAB2/src/sandevistan_description/launch/display.launch.py
+++ LAB2/src/sandevistan_description/launch/display.launch.py
@@ -7,46 +7,20 @@ from launch_ros.actions import Node
 import sys
 
 def generate_launch_description():
-
-    ### How to get path of a file from "install" directory ###
-    #
-    ## If our custom package [package_name] consists of a folder [folder_name].
-    ## And inside that folder, there is a file name [file_name.extension].
-    ## This is how to get full path to the file (in a form of string)
-
-    # path_to_package = get_package_share_directory(package_name)
-    # sub_folder = 'folder_name'
-    # file_name = 'file_name.extension'
-    # file_path = os.path.join(path_to_package,sub_folder,file_name) 
-    
-    ## You must build the custom package before. Otherwise, these codes will not find the file
-    # in "install" directory
-
     package_path = get_package_share_directory('sandevistan_description')
     rviz_file_path = os.path.join(package_path, 'config', 'sandevistan_kinematics.rviz')
     robot_desc_path = os.path.join(package_path, 'robot', 'sandevistan.urdf')
     
-    ### How to run rviz2 ###
-    #
-    ## With -d rviz_file_path, you can load the previous setting of rviz
-    #
     rviz = Node(
        package='rviz2',
        executable='rviz2',
        name='rviz',
        arguments=['-d', rviz_file_path],
        output='screen')
-    
-    ### How to read URDF file ###
-    #
-    # Assume that you have the correct path [robot_desc_path]
+
     with open(robot_desc_path, 'r') as infp:
         robot_description = infp.read()
     
-    ### How to run robot_state_publisher ###
-    #
-    # You must have a valid robot_description
-    #
     robot_state_publisher = Node(package='robot_state_publisher',
                                   executable='robot_state_publisher',
                                   output='screen',
@@ -56,21 +30,6 @@ def generate_launch_description():
                                   ]
     )
 
-    # Joint State Publisher & GUI (for debugging)
-    # joint_state_publisher = Node(package='joint_state_publisher',
-    #                                 executable='joint_state_publisher',
-    #                                 name='joint_state_publisher'
-    # )
-    # joint_state_publisher_gui = Node(package='joint_state_publisher_gui',
-    #                                     executable='joint_state_publisher_gui',
-    #                                     name='joint_state_publisher_gui'
-    # )
-
-    ### How to add actions to launch description ###
-    #
-    # You must create an object called "LaunchDescription"
-    # and add actions one by one
-    #
     # Launch Description
     launch_description = LaunchDescription()
     toAddAction = [rviz, robot_state_publisher]
--- LAB2/src/sandevistan_kinematics/launch/display_with_kinematics_srv.launch.py
+++ LAB2/src/sandevistan_kinematics/launch/display_with_kinematics_srv.launch.py
@@ -8,13 +8,7 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 import sys
 
-
 def generate_launch_description():
-
-    ### How to launch another launch file ###
-    #
-    # You must specify the package, folder, and the name
-    #
     display = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             PathJoinSubstitution([FindPackageShare('sandevistan_description'),
@@ -26,31 +20,11 @@ def generate_launch_description():
     kinematics_server = Node(package='sandevistan_kinematics',
                              executable='kinematics_server.py',)
     
-    # q1, q2, q3, q4 = 0.0, 0.0, 0.0, 0.0
-                    
-    # srv_jointState = ExecuteProcess(
-    #     cmd=[[
-    #         'ros2 service call /set_joints sandevistan_kinematics_interfaces/srv/GetPosition ',
-    #         f'"{{joint: {{position: [{q1},{q2},{q3},{q4}]}}}}"'
-    #     ]],
-    #     shell=True)
-    
-    # [x, y, z] = [0.12, 0.3, 0.5]
-    # [gamma1, gamma2] = [-1, 1]
-    # q3 = 20.0
-    
-    # srv_IKSolve = ExecuteProcess(
-    #     cmd=[[
-    #         'ros2 service call /solve_ik sandevistan_kinematics_interfaces/srv/SolveIK ',
-    #         f'"{{position: {{x: {x}, y: {y}, z: {z}}}, armconfig: [{gamma1}, {gamma2}], jointconfig: {q3}}}"'
-    #     ]],
-    #     shell=True)
     # Launch Description
     launch_description = LaunchDescription()
-    launch_description.add_action(display)
-    launch_description.add_action(kinematics_server)
-    # launch_description.add_action(srv_jointState)
-    # launch_description.add_action(srv_IKSolve)
+    toAddAction = [display, kinematics_server]
+    for i in toAddAction:
+        launch_description.add_action(i)
     
     return launch_description
 
